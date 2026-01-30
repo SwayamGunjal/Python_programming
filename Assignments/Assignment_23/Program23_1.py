aa96d7a81bf160737a8e46a63424de060850b25b
Swayam Gunjal <swayamgunjal2@gmail.com>
2026-01-30 23:08:43 +0530

    Add Assignment_23 solutions (Q1-Q3)

--- Assignments/Assignment_23/Program23_1.py
+++ Assignments/Assignment_23/Program23_1.py
@@ -1,53 +1,36 @@
 ########################################################################################################################
 #
-#   Class name      :   Demo
-#   Description     :   Implement a class with two instance variables, one class variable,
-#                       constructor, and two instance methods to display instance data.
-#   Input           :   Integer, Integer
+#   Class name      :   BookStore
+#   Description     :   Implement a class with instance variables, a class variable,
+#                       constructor to initialize data and increment book count,
+#                       and a method to display book details.
+#   Input           :   Str, Str
 #   Output          :   Void
 #   Author          :   Swayam Satish Gunjal
 #   Date            :   30/01/2026
 #
 ########################################################################################################################
 
+class BookStore:
+    NoOfBooks = 0
 
-class Demo:
-    Value = 10
-
-    def __init__(self, A, B):
-        self.No1 = A
-        self.No2 = B
-
-    def fun(self):
-        print("Inside fun")
-        print("Instance Variable No1 :", self.No1)
-        print("Instance Variable No2 :", self.No2)
-
-    def gun(self):
-        print("Inside gun")
-        print("Instance Variable No1 :", self.No1)
-        print("Instance Variable No2 :", self.No2)
+    def __init__(self, name, author):
+        self.Name = name
+        self.Author = author
+        BookStore.NoOfBooks += 1  
+    
+    def Display(self):
+        print(f"{self.Name} by {self.Author}.")
+        print(f"No of books : {BookStore.NoOfBooks}")
 
 def main():
-    Val1 = 0
-    Val2 = 0
+    Obj1 = BookStore("Linux System Programming", "Robert Love")
 
-    print("Enter the first number :")
-    Val1 = int(input())
+    Obj1.Display()
 
-    print("Enter the second number :")
-    Val2 = int(input())
-    
-    Obj1 = Demo(Val1, Val2)
-    Obj2 = Demo(10, 50)
+    Obj2 = BookStore("C Programming", "Dennis Ritche")
 
-    Obj1.fun()
-
-    Obj2.fun()
-
-    Obj1.gun()
-
-    Obj2.gun()
+    Obj2.Display()
 
 if __name__ == "__main__":
     main()
