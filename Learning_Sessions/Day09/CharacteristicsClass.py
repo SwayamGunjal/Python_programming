4eb083438e8378258eb48174e345df44eb91c074
Swayam Gunjal <swayamgunjal2@gmail.com>
2026-02-08 20:11:18 +0530

    Add learning session for Day09

---
+++ Learning_Sessions/Day09/CharacteristicsClass.py
@@ -0,0 +1,13 @@
+import gc
+
+class Demo:
+    No1 = 10
+    No2 = 11
+    def __init__(self):
+        print("Inside Constructor")
+
+    def __del__(self):
+        print("Inside Destructor")
+
+print(Demo.No1)
+print(Demo.No2)
